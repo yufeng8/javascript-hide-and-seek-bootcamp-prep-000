c3e3a54ac0b8894d9a8fe74742f8b94b90c84686
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-12 15:39:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  const main = document.getElementById('app')
+  const main = document.getElementById('app')[0]
   const div = main.children[3]
   const className = div.getElementByClassName("target")
 }
