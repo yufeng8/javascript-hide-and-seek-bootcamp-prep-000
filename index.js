1887ec9e76e66a728042f4fe71fa9b37a7743f50
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-12 18:45:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function increaseRankBy(n) {
 
   const rankedLists = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   for (var i = 0; i < rankedLists.length; i++) {
-    console.log("rankedLists", rankedLists[i].textContent)
+    console.log(rankedLists[i].textContent)
 /*    console.log("!!")
     theString1 = rankedLists[i].replace("<li>", "")
     console.log("theString1", theString1)
