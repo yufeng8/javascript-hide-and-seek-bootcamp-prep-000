b2c11f82374742e4ec9e531dd5138876042acedd
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-12 18:10:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ function increaseRankBy(n) {
 function deepestChild() {
   const main = document.getElementById('app')
   const id = main.querySelector('#grand-node')
-  const 
+  const deepestChild = id.querySelectorAll('div')
 }
