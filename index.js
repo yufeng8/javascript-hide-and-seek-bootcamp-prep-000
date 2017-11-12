1243402a1b1e9e95b8b11eec0bd1a676b26154dc
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-12 15:20:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  const main = document.getElementById('app')
+  const div = main.children[3]
+  const className = div.getElementByclassName("target")
+}
+
+function increaseRankBy(n) {
+
+}
+
+function deepestChild() {
+
+}
