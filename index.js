7ee1364ccdc929e87b5c6c482646ef8d3b306a17
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-12 16:54:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ function getFirstSelector(selector) {
 function nestedTargetX() {
   console.log("foosd fksdjflsdkfj")
   console.log('!!!!!!')
-
+/*
   const main = document.getElementById('app')
   console.log('main', main);
   const id= main.querySelector('#nested')
   console.log('id', id)
   const className = id.querySelector('.target')
   console.log('classname', className)
-  //const className = div.getElementByClassName("target")
+  //const className = div.getElementByClassName("target")*/
 }
 
 function increaseRankBy(n) {
