8bba62af07feafe9834a8c39cff84c468c1985ac
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-12 17:56:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const rankedLists = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   for (var i = 0; i < rankedLists.length; i++) {
-    rankedLists[i].innerHTML = (i + 1 + n).toString()
+    rankedLists[i].innerHTML = (rankedLists[i] + n).toString()
     console.log(rankedLists[i].innerHTML)
   }
 }
